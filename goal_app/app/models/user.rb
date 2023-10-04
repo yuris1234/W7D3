83fb549009cede187ce91cd7aca416c3f423ca96
yuris <yls4@cornell.edu>
2023-10-04 15:26:12 -0400

    figured out session token error

--- goal_app/app/models/user.rb
+++ goal_app/app/models/user.rb
@@ -1,8 +1,30 @@
 class User < ApplicationRecord
     # FIGVAPEBR
+    before_validation :ensure_session_token
     validates :username, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true
     # validates :password, length:{minimum: 6}
 
+    def is_password?(password)
 
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    private
+
+    def generate_unique_session_token
+        SecureRandom::urlsafe_base64
+    end
 end
--- goal_app/spec/factories/users.rb
+++ goal_app/spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user do
     username{Faker::Movies::HarryPotter.character}
     password{"password"}
+    # session_token{"kasdjfk1209"}
   end
 end
